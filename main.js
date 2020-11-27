37bda539687b0da4d21169705aacc3ee9af823fc
FRANCESCO <OLIVOTTI>
2020-11-27 16:09:57 +0100

    NextButton 1.0

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var PreviewButton = $('.fa-angle-left');
+    var NextButton = $('.fa-angle-right');
+  
+    // Cliccando sulla freccia di dx scorro in avanti lo slider
+    // e coloro correttamente i pallini
+    NextButton.click(function () {
+      NextImage();
+    });
+  
+    // Cliccando sulla freccia di sx scorro indietro lo slider
+    // e coloro correttamente i pallini
+    PreviewButton.click(function () {
+      PreviewImage();
+    });
+  
+  
+  });
+  
+  //FUNZIONI PER LO SLIDER
+  
+  function NextImage() {
+    var imgActive = $('img.active');
+    var imgFirst = $('img.first');
+    var dotActive = $('i.fa-circle.active');
+    var dotFirst = $('i.fa-circle.first');
+  
+    imgActive.removeClass('active');
+    dotActive.removeClass('active');
+  
+    if (imgActive.hasClass('last')) {
+      imgFirst.addClass('active');
+      dotFirst.addClass('active');
+    } else {
+      imgActive.next('img').addClass('active');
+      dotActive.next('i').addClass('active');
+    }
+  };
+  
+  
